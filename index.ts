a584df4d8fd0b04c472dd6abce2a346de9b83fdb
ASHWANI KR RAI <114147502+ashwani3206@users.noreply.github.com>
2024-10-20 15:15:42 +0530

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,10 +1,50 @@
-export { default as Home } from "./Home";
-export { default as Explore } from "./Explore";
-export { default as Saved } from "./Saved";
-export { default as CreatePost } from "./CreatePost";
-export { default as Profile } from "./Profile";
-export { default as UpdateProfile } from "./UpdateProfile";
-export { default as EditPost } from "./EditPost";
-export { default as PostDetails } from "./PostDetails";
-export { default as LikedPosts } from "./LikedPosts";
-export { default as AllUsers } from "./AllUsers";
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/assets/icons/people.svg",
+    route: "/all-users",
+    label: "People",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Create Post",
+  },
+];
+
+export const bottombarLinks = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Create",
+  },
+];
